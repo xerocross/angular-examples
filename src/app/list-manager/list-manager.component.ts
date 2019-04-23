1482cba31d51d063cf395267eaa4928e7f4e1254
xerocross <nonya@business.com>
2019-04-23 02:17:43 -0400

    pipe the input values through some filters before listing them

--- src/app/list-manager/list-manager.component.ts
+++ src/app/list-manager/list-manager.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Subject } from "rxjs";
+import { debounceTime, distinctUntilChanged, filter, map } from "rxjs/operators";
 
 @Component({
   selector: 'app-list-manager',
@@ -8,13 +9,19 @@ import { Subject } from "rxjs";
 })
 export class ListManagerComponent implements OnInit {
   textInputs$ : Subject<string>;
-
+  inputValues : string[] = [];
   constructor() { }
 
   ngOnInit() {
     this.textInputs$ = new Subject<string>();
-    this.textInputs$.subscribe(val=> {
+    this.textInputs$
+    .pipe(debounceTime(600))
+    .pipe(map(val => val.trim()))
+    .pipe(distinctUntilChanged())
+    .pipe(filter(val => val.length > 0))
+    .subscribe(val=> {
       console.log("from observable in parent: " + val);
+      this.inputValues.push(val);
     });
   }
 
